63cf27ae890ca850655765d74f5e3282d734f817
Bárbara Hedwig von Kostrisch <hedwigvonkotrisch@hotmail.com>
2024-10-19 18:44:53 -0300

    Making pages that lists the directors and writers and display the movies, also the count and a flow between them

--- learning-adonis/app/controllers/movies_controller.ts
+++ learning-adonis/app/controllers/movies_controller.ts
@@ -5,11 +5,15 @@ export default class MoviesController {
   async index({ view }: HttpContext) {
     const comingSoon = await Movie.query()
       .apply((scope) => scope.notReleased())
+      .preload('director')
+      .preload('writer')
       .whereNotNull('releasedAt')
       .orderBy('releasedAt')
       .limit(3)
     const recentlyReleased = await Movie.query()
       .apply((scope) => scope.released())
+      .preload('director')
+      .preload('writer')
       .orderBy('releasedAt', 'desc')
       .limit(9)
     return view.render('pages/home', { comingSoon, recentlyReleased })
@@ -17,6 +21,10 @@ export default class MoviesController {
 
   async show({ view, params }: HttpContext) {
     const movie = await Movie.findByOrFail('slug', params.slug)
+
+    await movie.load('director')
+    await movie.load('writer')
+
     return view.render('pages/movies/show', { movie })
   }
 }
